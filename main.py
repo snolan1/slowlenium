29ec8359a9426c557faafda498ae8d9ef939f551
Steve Nolan <stevenolanmail@gmail.com>
2024-08-22 15:19:53 -0700

    Set up dunder for direct run

--- main.py
+++ main.py
@@ -2,9 +2,22 @@
 
 from selenium import webdriver
 
-
+import time
 
 def initialize_driver():
     driver = webdriver.Chrome()
     driver.maximize_window()
     return driver
+
+def wait_indefinitely(driver):
+    while True:
+        time.sleep(86400)
+
+def main():
+
+    driver = initialize_driver()
+    wait_indefinitely(driver)
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
