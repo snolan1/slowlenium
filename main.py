f826fdaeee40ee5f813e053b06e4b685b66087bb
Steve Nolan <stevenolanmail@gmail.com>
2024-08-22 15:30:46 -0700

    Stub out link processing

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 #! /usr/bin/env python3
 
-from selenium import webdriver
-
 import time
+import random
+import pyautogui
+from selenium import webdriver
 
 def initialize_driver():
     driver = webdriver.Chrome()
     driver.maximize_window()
     return driver
+
+def process_links(driver):
+    with open('testlinks.txt') as links:
+        for link in links:
+            time.sleep(random.randint(0,2))
+            driver.switch_to.new_window("tab")
+            print(f"Loading: {link.rstrip()}...")
+            try:
+                driver.get(link)
+                print(f"Loaded: {link.rstrip()}.")
+                pyautogui.press('tab', presses=4)
+            except Exception as e:
+                print(f"Encountered error:\n{e}")
+                pass
+            time.sleep(1)
 
 def wait_indefinitely(driver):
     while True:
@@ -16,6 +32,7 @@ def wait_indefinitely(driver):
 def main():
 
     driver = initialize_driver()
+    process_links(driver)
     wait_indefinitely(driver)
     driver.quit()
 
